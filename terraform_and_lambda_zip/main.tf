c2d62139ee3286ea0f170f7af04b1c75d60f299d
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-01-20 22:34:56 -0500

    Update and rename fresh_tech_talent_ats.tf to main.tf

---
+++ terraform_and_lambda_zip/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  cloud {
+    organization = "TerraformVig"
+
+    workspaces {
+      name = "provisioners"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "template_file" "user_data" {
+  template = file("./userdata.yml")
+}
+
+resource "aws_instance" "amazon-linux-2-with-nginx-and-flask-application" {
+  ami                         = var.ami
+  instance_type               = var.instance-type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.vpc_security_group_ids]
+  associate_public_ip_address = true
+  user_data                   = data.template_file.user_data.rendered
+
+  #user_data = "${file("user-data-fresh-tech-talent-ats.sh")}""
+
+  tags = {
+    Name = "Amazon Linux 2 with Nginx server and Flask app running on port 8080"
+  }
+}
